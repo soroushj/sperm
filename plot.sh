4316809cb782af9d2dfc547c3f343982d9a0262f
Soroush Javadi <soroush.javadi@gmail.com>
2019-01-19 19:27:04 +0330

    Plots

--- plot.sh
+++ plot.sh
@@ -1,51 +1,63 @@
 #!/usr/bin/env bash
 
-# acc-all
+declare -a keys=("all" "acrosome" "head" "vacuole")
+
+for k in "${keys[@]}"
+do
+  if [ "$k" == "all" ]; then
+    n=6
+  else
+    n=2
+  fi
+
+# acc
 gnuplot -e "
 set terminal pdf;
-set output 'saved-results/results-plot/acc-all.pdf';
+set output 'saved-results/results-plot/acc-$k.pdf';
 set datafile separator ',';
 set key autotitle columnhead;
 set xlabel 'Iterations';
 set ylabel 'Accuracy';
 set yrange [0:1];
 set key right bottom;
-plot for [i=1:6] 'saved-results/results/acc-all.csv' using 0:i smooth bezier;
+plot for [i=1:$n] 'saved-results/results/acc-$k.csv' using 0:i smooth bezier;
 "
 
-# loss-all
+# loss
 gnuplot -e "
 set terminal pdf;
-set output 'saved-results/results-plot/loss-all.pdf';
+set output 'saved-results/results-plot/loss-$k.pdf';
 set datafile separator ',';
 set key autotitle columnhead;
 set xlabel 'Iterations';
 set ylabel 'Loss';
 set yrange [0:1];
-plot for [i=1:6] 'saved-results/results/loss-all.csv' using 0:i smooth bezier;
+plot for [i=1:$n] 'saved-results/results/loss-$k.csv' using 0:i smooth bezier;
 "
 
-# acc-all-no-aug
+# acc-no-aug
 gnuplot -e "
 set terminal pdf;
-set output 'saved-results/results-plot/acc-all-no-aug.pdf';
+set output 'saved-results/results-plot/acc-$k-no-aug.pdf';
 set datafile separator ',';
 set key autotitle columnhead;
 set xlabel 'Iterations';
 set ylabel 'Accuracy';
 set yrange [0:1];
 set key right bottom;
-plot for [i=1:6] 'saved-results/results/acc-all-no-aug.csv' using 0:i smooth bezier;
+plot for [i=1:$n] 'saved-results/results/acc-$k-no-aug.csv' using 0:i smooth bezier;
 "
 
-# loss-all-no-aug
+# loss-no-aug
 gnuplot -e "
 set terminal pdf;
-set output 'saved-results/results-plot/loss-all-no-aug.pdf';
+set output 'saved-results/results-plot/loss-$k-no-aug.pdf';
 set datafile separator ',';
 set key autotitle columnhead;
 set xlabel 'Iterations';
 set ylabel 'Loss';
 set yrange [0:4];
-plot for [i=1:6] 'saved-results/results/loss-all-no-aug.csv' using 0:i smooth bezier;
+plot for [i=1:$n] 'saved-results/results/loss-$k-no-aug.csv' using 0:i smooth bezier;
 "
+
+done
