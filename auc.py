77a005aac2d9c4839928906e1687ad2fa0a91811
Soroush Javadi <soroush.javadi@gmail.com>
2019-02-25 23:46:21 +0330

    auc

--- auc.py
+++ auc.py
@@ -4,18 +4,22 @@ import os
 import csv
 from sklearn.metrics import roc_auc_score
 
-predictions_dir = 'saved-results/predictions'
+predictions_dirs = [
+    'saved-results/predictions',
+    'saved-results/ghasemian/predictions',
+]
 
-for filename in os.listdir(predictions_dir):
-    label = filename[3]
-    in_file = open(os.path.join(predictions_dir, filename))
-    reader = csv.reader(in_file)
-    next(reader)
-    y_true = []
-    y_score = []
-    for row in reader:
-        y_true.append(int(row[2]))
-        y_score.append(float(row[1]))
-    auc = roc_auc_score(y_true, y_score)
-    print('{}: {:.4f}'.format(label, auc))
-    in_file.close()
+for predictions_dir in predictions_dirs:
+    for filename in os.listdir(predictions_dir):
+        with open(os.path.join(predictions_dir, filename)) as f:
+            reader = csv.reader(f)
+            next(reader)
+            y_true = []
+            y_score = []
+            for row in reader:
+                if int(row[0]) > 1240:
+                    y_true.append(float(row[2]))
+                    y_score.append(float(row[1]))
+            assert len(y_true) == 300
+            auc = roc_auc_score(y_true, y_score)
+            print('{}: {:.4f}'.format(filename, auc))
