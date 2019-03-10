8bdb8f9438393b74058657400a57b910b0d512d6
Soroush Javadi <soroush.javadi@gmail.com>
2019-03-11 02:15:34 +0330

    fix

--- ttest.py
+++ ttest.py
@@ -45,7 +45,7 @@ for label, files in sorted(prediction_files.items()):
     print('contingency table, {}:\n{}\t{}\n{}\t{}'.format(label, ct[0][0], ct[0][1], ct[1][0], ct[1][1]))
     print()
     tt = ttest_rel(a, b)
-    mc = mcnemar(ct, exact=False)
+    mc = mcnemar(ct, exact=False, correction=False)
     print(' t-test, {}\t\tstat={:+.2f}\tpval={:.6f}\t\tstat={}\tpval={}'.format(label, tt.statistic, tt.pvalue, tt.statistic, tt.pvalue))
     print('mcnemar, {}\t\tstat={:+.2f}\tpval={:.6f}\t\tstat={}\tpval={}'.format(label, mc.statistic, mc.pvalue, mc.statistic, mc.pvalue))
     print()
