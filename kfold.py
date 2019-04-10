7bf84994d5571661a7d9a6438e82887552a2cec8
Soroush Javadi <soroush.javadi@gmail.com>
2019-04-10 17:12:16 +0430

    kfold

--- kfold.py
+++ kfold.py
@@ -4,13 +4,21 @@ import os
 
 images_dir = 'data/images/gray'
 num_folds = 5
+train_size = 1000
 
 images = os.listdir(images_dir)
 images.sort()
-n_rotate = len(images) // num_folds
+fold_size = len(images) // num_folds
 
 def get_sets(k):
-    train = []
-    valid = []
-    test = []
+    assert 0 <= k < num_folds
+    a = k * fold_size
+    b = (k + 1) * fold_size
+    test = images[a:b]
+    non_test = images[:a] + images[b:]
+    train = non_test[:train_size]
+    valid = non_test[train_size:]
     return train, valid, test
+
+for k in range(num_folds):
+    print(get_sets(k))
